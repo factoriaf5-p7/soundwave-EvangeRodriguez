c72e477440fb0c5df84d3ff12a622698aefa3835
EvangeRodriguez <eva.irodriguez93@gmail.com>
2023-07-12 18:31:42 +0200

    estilo de formulario listo

--- soundwave/src/components/Form/Form.tsx
+++ soundwave/src/components/Form/Form.tsx
@@ -1,25 +1,28 @@
 
 import { Button, Form, Input, } from 'antd';
 import { Link } from 'react-router-dom';
+import './Form.css'
 
 export const Forms = () => {
-    return (
-    <>
-      <Form layout="vertical" autoComplete="off" >
-        <Form.Item name="name" label="Name:">
-          <Input />
+  return (
+    <div className='forms-style'>
+      <Form layout="vertical" autoComplete="off" className="form">
+        <Form.Item name="name" label="Name:" className="form-item">
+          <Input className="form-input" />
         </Form.Item>
-        <Form.Item name="email" label="Email:">
-          <Input/>
+        <Form.Item name="email" label="Email:" className="form-item">
+          <Input className="form-input" />
         </Form.Item>
-        <Form.Item name="password" label="Password:">
-          <Input/>
+        <Form.Item name="password" label="Password:" className="form-item">
+          <Input className="form-input" />
         </Form.Item>
       </Form>
-      <Link to='/404:Not Found'>
-      <Button type="primary">Join Now</Button> 
-      </Link> 
-    </>
+      <Link to="/404:Not Found" className="form-link">
+        <Button type="primary" className="form-button">
+          Join Now
+        </Button>
+      </Link>
+    </div>
   );
 };
-
+ 
