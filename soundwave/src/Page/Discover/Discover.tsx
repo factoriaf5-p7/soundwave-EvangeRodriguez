12d59a95eaaa3ff147ea773ab4ed9d76c8046682
EvangeRodriguez <eva.irodriguez93@gmail.com>
2023-07-13 00:20:03 +0200

    responsive listo

--- soundwave/src/Page/Discover/Discover.tsx
+++ soundwave/src/Page/Discover/Discover.tsx
@@ -7,18 +7,18 @@ import './Discover.css'
 export default function Discover() {
   return (
     <div>
-      <Navbar />
-      <div className="container-discover"> 
-      <div className="content-discover">
-          <div className="text-discover">
-            <h1 className="discover">Discover new music</h1>
-            <MenuDiscover />
-            <p className="parrafo">By joining you can benefit by listening to the latest albums released</p>
-          </div>
-          <img src={covers} className="imagen" alt="Album covers" />
+      <Navbar/>
+        <div className="container-discover"> 
+            <div className="content-discover">
+              <div className="text-discover">
+                  <h1 className="discover">Discover new music</h1>
+                  <MenuDiscover />
+                  <p className="parrafo">By joining you can benefit by listening to the latest albums released</p>
+              </div>
+              <img src={covers} className="imagen" alt="Album covers" />
+            </div>
+          <Footer />
         </div>
-      <Footer />
-      </div>
     </div>
   );
 }
